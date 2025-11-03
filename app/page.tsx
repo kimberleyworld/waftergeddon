14e419c4725c5852d5152dff467f468e65f2b1ac
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2025-11-03 16:45:42 +0000

    added fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Waftergeddon site coming soon...</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 dark:bg-black p-8">
+      <div className="text-center space-y-8 max-w-4xl">
+        <h1 className="font-cloister text-6xl mb-8 text-center">
+          Waftergeddon
+        </h1>
+        <h2 className="text-l">
+            website coming soon
+          </h2>
+      </div>
     </div>
   );
 }
